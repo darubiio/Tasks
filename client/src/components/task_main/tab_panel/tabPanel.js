439acb5e3aaee1579a07c3e8138196785f4b6ec0
Alejandro Rubio <darubiio97@icloud.com>
2021-10-18 12:17:05 +0200

    Bug fixes

--- client/src/components/task_main/tab_panel/tabPanel.js
+++ client/src/components/task_main/tab_panel/tabPanel.js
@@ -1,19 +1,19 @@
 import React from 'react'
-// import { useParams } from 'react-router';
 import { BackBtn } from '../backbtn';
-// import { Taskslists } from '../task/taskslists';
+import { Taskslists } from '../task/taskslists';
 import { ScaleFade } from '@chakra-ui/transition';
 import { Box, GridItem, Heading } from '@chakra-ui/layout';
 import { useQueryParams } from '../../../hook/searchParams';
+import * as Query from '../../../fetching/query'
 import { useQuery } from '@apollo/client';
 import { useParams } from 'react-router';
-import * as Query from '../../../fetching/query'
 import { Tasksl } from '../task/tasksl';
 
 export const TabPanel = () => {
   let { tab } = useParams();
   let params = useQueryParams();
   const { loading, error, data } = useQuery(Query[tab.toUpperCase()]);
+  const date = new Date().toDateString();
 
   return (
     <GridItem p={[1, 5]} colSpan={[5, 4]}>
@@ -21,7 +21,12 @@ export const TabPanel = () => {
         <Box h='88vh' overflow='scroll'>
           <BackBtn />
           <Heading size='lg' color='orange.500'>{params.get('name')}</Heading>
-          <Tasksl state={tab} data={data} error={error} loading={loading} />
+          {tab === 'my_day' ?
+            <Heading as="h6" size="xs">{date}</Heading> : ''}
+          {tab !== 'all_tasks' ?
+            <Tasksl state={tab} data={data} error={error} loading={loading} /> :
+            <Taskslists state='tasks' loading={loading} error={error} data={data} />
+          }
         </Box>
       </ScaleFade>
     </GridItem>
--- client/src/components/task_main/taskMain.js
+++ client/src/components/task_main/taskMain.js
@@ -1,13 +1,6 @@
 import React from 'react';
 
 import { Main } from './main';
-// import { All } from './todo/all';
-// import { MyDay } from './myDay/md';
-// import { Tasks } from './tasks/tks';
-// import { Planned } from './planned/plnd';
-// import { Asigned } from './asigned/asigned';
-// import { Important } from './important/imp';
-// import { Completed } from './completed/compd';
 import { Route, Switch } from 'react-router';
 import { List } from './list/list';
 import { Login } from '../forms/login';
@@ -20,16 +13,9 @@ export const TaskMain = () => {
     <Switch>
       <PrivateRoute path='/list/:id' children={<List />} />
       <PrivateRoute path='/:tab' children={<TabPanel />} />
-      {/* <PrivateRoute children={<Important />} /> */}
-      {/* <PrivateRoute path='/completed' children={<Completed />} /> */}
-      {/* <PrivateRoute path='/planned' children={<Planned />} />
-      <PrivateRoute path='/asigned' children={<Asigned />} />
-      <PrivateRoute path='/my-day' children={<MyDay />} />
-      <PrivateRoute path='/tasks' children={<Tasks />} />
-      <PrivateRoute path='/all' children={<All />} /> */}
-      <PrivateRoute path='/' children={<Main />} />
       <Route path='/signup' component={SignUp} />
       <Route path='/login' component={Login} />
+      <PrivateRoute path='/' children={<Main />} />
     </Switch>
   )
 };
